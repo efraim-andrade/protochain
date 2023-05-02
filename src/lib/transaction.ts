76a0a38e8d9b74a1ff0e2abd4778507d2af23014
Efraim Andrade <efraim_andrade@Efraims-MacBook-Air.local>
2023-05-02 20:16:40 -0300

    feat: added the transaction class

---
+++ src/lib/transaction.ts
@@ -0,0 +1,44 @@
+import sha256 from "crypto-js/sha256";
+import TransactionType from "./transactionTypes";
+import Validation from "./validation";
+
+/**
+ * Transaction class
+ */
+export default class Transaction {
+  hash: string;
+  data: string;
+  timestamp: number;
+  type: TransactionType;
+
+  constructor(tx?: Transaction) {
+    this.type = tx?.type || TransactionType.REGULAR;
+    this.timestamp = tx?.timestamp || Date.now();
+    this.data = tx?.data || "";
+    this.hash = tx?.hash || this.getHash();
+  }
+
+  /**
+   *  Get the hash of the transaction
+   * @returns {string} Hash of the transaction
+   */
+  getHash(): string {
+    return sha256(this.type + this.data + this.timestamp).toString();
+  }
+
+  /**
+   *  Validate the transaction
+   * @returns {Validation} Validation object
+   */
+  isValid(): Validation {
+    if (this.hash !== this.getHash()) {
+      return new Validation(false, "Invalid hash");
+    }
+
+    if (!this.data) {
+      return new Validation(false, "Invalid data");
+    }
+
+    return new Validation();
+  }
+}
